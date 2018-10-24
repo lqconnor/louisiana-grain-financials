a0b8ec82a65d5b8fb5e56f0a3cab5a0ad2506e3a
lqconnor <connorql@gmail.com>
2018-10-24 14:34:16 -0500

    Financial stress initial data read in and data explore.

---
+++ fin_stress_LA.R
@@ -0,0 +1,26 @@
+
+# Pre - Amble -----------------------------------------------------------------------------
+rm(list = ls())
+cat("\f")
+getwd()
+
+Sys.setenv(NASSQS_TOKEN = readLines(".secret"))
+
+pckgs <- c("tidyverse", "stargazer", "rnassqs")
+lapply(pckgs, library, character.only = TRUE)
+
+
+income <- read_csv("../../../Data/FarmIncome_WealthStatisticsData_August2018.csv") %>%
+  filter(str_detect(VariableDescriptionTotal, "current"))
+
+
+# Get and clean NASS API data -------------------------------------------------------------
+# List parameters of interest to feed to rnassqs package
+params = list(source_desc = "SURVEY", 
+              state_alpha = "LA",
+              year = "2017")
+
+# County Cash Rent
+# Feed parameters to rnassqs package
+asset <- nassqs(params = params) %>%
+  select(state_name, state_alpha, agg_level_desc, short_desc, Value, year)  # keep variables of interest
